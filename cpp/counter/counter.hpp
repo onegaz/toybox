b65ef2f1b594a2a5a4bfee200c5947cfdd5ecfae
onegaz <onegaz@github.com>
2017-06-17 17:36:39 -0700

    refactor

--- cpp/counter/counter.hpp
+++ cpp/counter/counter.hpp
@@ -1,86 +1,94 @@
 #ifndef __counter_hpp__
 #define __counter_hpp__
-#include <cstdio>
 #include <string.h>
-#include <stdlib.h>
 #include <boost/current_function.hpp>
+#include <iostream>
+
+struct object_counter
+{
+	char name[120];
+	size_t count;
+	bool empty() const
+	{
+		return name[0]==0;
+	}
+	void inc()
+	{
+		__sync_fetch_and_add(&count, 1);
+	}
+	void dec ()
+	{
+		__sync_fetch_and_sub(&count, 1);
+	}
+};
 
 template<int N>
 struct crtp_counter_store
 {
-	size_t store;
+	int index;
 	crtp_counter_store(const char* FN)
 	{
-		store = 0;
-		reg(FN, &store);
-	}
-
-	static void reg(const char* FN, size_t* cnt)
-	{
-		int pos = next();
-		get_counter_pointers()[pos] = cnt;
-		sprintf(get_counter_name(pos), "%s", FN);
-	}
-
-	static size_t** get_counter_pointers()
-	{
-		static size_t* ptrs[N];
-		return ptrs;
-	}
-
-	static char* get_counter_name(int n)
-	{
-		static char names[N][256];
-		return names[n];
-	}
-
-	static int next()
-	{
 		static int next_pos=0;
-		return __sync_fetch_and_add(&next_pos, 1);
+		index = __sync_fetch_and_add(&next_pos, 1);
+		if (index >= N)
+			index = N - 1;
+		strncpy(get_counter().name, FN, sizeof(get_counter().name)-1);
 	}
 
-	static void dump()
+	static object_counter& get_counter(int n)
 	{
-		int i = 0;
-		size_t** cnts = get_counter_pointers();
-		int name_col_width = 8*2;
-		write(STDOUT_FILENO, "Type", strlen("Type"));
-		int tabs = name_col_width/8 - strlen(get_counter_name(i))/8;
-		for(int i=0; i<tabs; i++)
-			write(STDOUT_FILENO, "\t", 1);
-		printf("Counter\n");
-		for (int i=0; i<N; i++)
-		{
-			if (cnts[i])
-			{
-				write(STDOUT_FILENO, get_counter_name(i), strlen(get_counter_name(i)) );
-				int tabs = name_col_width/8 - strlen(get_counter_name(i))/8;
-				for(int i=0; i<tabs; i++)
-					write(STDOUT_FILENO, "\t", 1);
-				printf("%zu\n", *cnts[i]);
-			}
-		}
+		static object_counter store[N];
+		return store[n];
 	}
+
+	object_counter& get_counter()
+	{
+		return get_counter(index);
+	}
+
+    static void output(std::ostream& os)
+    {
+		int i = 0;
+		int name_col_width = 8*2;
+		os << "Type";
+		int tabs = name_col_width/8 - strlen("Type")/8;
+		for(int i=0; i<tabs; i++)
+			os << "\t";
+		os << "Counter\n";
+    	for (int i=0; i<N; i++)
+    	{
+			if (get_counter(i).empty())
+				break;
+			os << get_counter(i).name;
+			tabs = name_col_width/8 - strlen(get_counter(i).name)/8;
+			for(int j=0; j<tabs; j++)
+				os << "\t";
+			os << get_counter(i).count	<< "\n";
+    	}
+		if ( i >= N-1 )
+			os <<"Some counters may be missing, please increase value of N!\n";
+    }
 };
+
+typedef crtp_counter_store<2048> crtp_counter_store_type;
 
 template <typename T>
 struct crtp_counter
 {
 	crtp_counter()
     {
-    	__sync_fetch_and_add( &get_counter(), 1 );
+		get_store().get_counter().inc();
     }
 
-    static size_t& get_counter()
+    static crtp_counter_store_type& get_store()
     {
-    	static crtp_counter_store<2048> ccs(get_type_name());
-    	return ccs.store;
+    	static crtp_counter_store_type ccs(get_type_name());
+    	return ccs;
     }
 
     ~crtp_counter()
     {
-    	__sync_fetch_and_sub( &get_counter(), 1 );
+    	get_store().get_counter().dec();
     }
 
     static const char* get_type_name(void)
@@ -88,11 +96,23 @@ struct crtp_counter
     	static const char gcc_prefix[] = "static const char* crtp_counter<T>::get_type_name() [with T = ";
     	static const char gcc_postfix[] = "]";
     	static const char clang_prefix[] = "static const char *crtp_counter<";
-    	static const char clang_postfix[] = ">::get_type_name()";
+    	static const char clang_postfix[] = ">::get_type_name";
     	static char name[sizeof(BOOST_CURRENT_FUNCTION)-sizeof(clang_prefix)];
     	if (!name[0])
     	{
-    		// printf("%s\n", BOOST_CURRENT_FUNCTION);
+#ifdef _MSC_VER
+        	static const char msvc_prefix[] = "const char *__cdecl crtp_counter<";
+        	static const char msvc_postfix[] = ">::get_type_name(void)";
+//    		_MSC_VER 1900	http://rextester.com/l/cpp_online_compiler_visual
+//    		BOOST_CURRENT_FUNCTION const char *__cdecl crtp_counter<int>::get_type_name(void)
+//    		__FUNCSIG__ const char *__cdecl crtp_counter<int>::get_type_name(void)
+//    		const char *__cdecl crtp_counter<int>::get_type_name(void)
+    		const char* start = strstr(__FUNCSIG__, msvc_prefix) + strlen(msvc_prefix);
+    		const char* endpos = strstr(__FUNCSIG__, msvc_postfix);
+    		int len = endpos - start;
+    		memcpy(name, start, len);
+    		name[len] = 0;
+#else
     		if (strstr(BOOST_CURRENT_FUNCTION, gcc_prefix)==nullptr)
     		{
         		strcpy(name, BOOST_CURRENT_FUNCTION + sizeof(clang_prefix)-1);
@@ -104,6 +124,7 @@ struct crtp_counter
         		strcpy(name, BOOST_CURRENT_FUNCTION + sizeof(gcc_prefix)-1);
         		name[strlen(name)-strlen(gcc_postfix)] = 0;
     		}
+#endif
     	}
     	return name;
     }
--- cpp/counter/main.cpp
+++ cpp/counter/main.cpp
@@ -36,18 +36,18 @@ int main()
 	vector<Derived2*> derived2_objs;
 	for(int i=0; i<4; i++)
 		base_objs.push_back(new Base);
-	for(int i=0; i<10; i++)
+	for(int i=0; i<6; i++)
 		derived_objs.push_back(new Derived);
-	for(int i=0; i<100; i++)
+	for(int i=0; i<8; i++)
 		derived2_objs.push_back(new Derived2);
 
 	cout << "Object counters after ctor\n";
-	crtp_counter_store<2048>::dump();
+	crtp_counter_store_type::output(std::cout);
 	std::for_each(base_objs.begin(), base_objs.end(), std::default_delete<Base>());
 	std::for_each(derived_objs.begin(), derived_objs.end(), std::default_delete<Derived>());
 	std::for_each(derived2_objs.begin(), derived2_objs.end(), std::default_delete<Derived2>());
 	cout << "Object counters after dtor\n";
-	crtp_counter_store<2048>::dump();
+	crtp_counter_store_type::output(std::cout);
 
 	cout  << "Use Curiously Recurring Template Pattern (CRTP) to count live objects\n"
 		  << "Build with Compiler: " << BOOST_COMPILER << std::endl
