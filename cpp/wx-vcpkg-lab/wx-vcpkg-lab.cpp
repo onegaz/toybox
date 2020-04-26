c774e0406cf70d1bcb0389064fb02d042a6de44b
onegaz <onega@hotmail.com>
2020-04-25 19:57:34 -0700

    Create wx-vcpkg-lab.cpp

---
+++ cpp/wx-vcpkg-lab/wx-vcpkg-lab.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <wx/wx.h>
+
+int main()
+{
+    wxString wxHelloWorld = "Hello vcpkg!\n";
+    std::cout << wxHelloWorld;
+}
+
+// method 1:
+// rm CMakeCache.txt ; VCPKG_TARGET_TRIPLET=x64-osx CMAKE_TOOLCHAIN_FILE=~/oss/vcpkg/scripts/buildsystems/vcpkg.cmake cmake -S .. -B .
+
+// method 2:
+// rm CMakeCache.txt ; wxWidgets_INCLUDE_DIRS=~/oss/vcpkg/packages/wxwidgets_x64-osx/include/wx-3.1 wxWidgets_LIBRARIES=~/oss/vcpkg/packages/wxwidgets_x64-osx/lib  cmake -S .. -B .
