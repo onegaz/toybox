81401cf83ef0ca90dac005529de10ecc54161918
onegaz <onegaz@github.com>
2020-04-26 20:58:27 -0700

    minor change

--- cpp/wxterm/build.sh
+++ cpp/wxterm/build.sh
@@ -2,6 +2,16 @@
 
 rm CMakeCache.txt
 
+cmake -DCMAKE_TOOLCHAIN_FILE=~/oss/vcpkg/scripts/buildsystems/vcpkg.cmake \
+	-DwxWidgets_CONFIG_EXECUTABLE=~/oss/vcpkg/buildtrees/wxwidgets/x64-osx-rel/wx-config \
+	-DwxWidgets_LIB_DIR=~/oss/vcpkg/buildtrees/wxwidgets/x64-osx-rel/lib \
+	-DwxWidgets_USE_STATIC=ON -DwxWidgets_USE_UNICODE=ON \
+	..
+
+cmake --build .
+
+exit 0
+
 BOOST_ROOT="${BOOST_ROOT:-~/Downloads/boost_1_69_0}"
 BOOST_INCLUDEDIR="${BOOST_INCLUDEDIR:-~/Downloads/boost_1_69_0}"
 
@@ -22,3 +32,4 @@ cmake -DBOOST_INCLUDEDIR=~/Downloads/boost_1_69_0 \
 -DBOOST_ROOT=~/Downloads/boost_1_69_0 \
 -DZLIB_ROOT=~/oss/zlib \
  -DwxWidgets_CONFIG_EXECUTABLE=~/Downloads/wxWidgets-3.1.2/bld/wx-config ..
+ 
